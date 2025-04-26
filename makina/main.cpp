62c6de0de7c8e0ad7b009afb97a8e0f16c24c20a
andi <andikojanaj321@gmail.com>
2025-04-26 10:21:22 +0200

    Refactor menu display for better readability and add spacing in input prompts

--- makina/main.cpp
+++ makina/main.cpp
@@ -59,11 +59,11 @@ public:
 
 void shfaqMenu()
 {
-    cout << "1 Shto makine: " << endl;
-    cout << "2 Shto motor: " << endl;
-    cout << "3 Shto kamion: " << endl;
-    cout << "4 Dil: " << endl;
-    cout << " Zgjidhni: " << endl;
+    cout << "\t 1 Shto makine: " << endl;
+    cout << "\t 2 Shto motor: " << endl;
+    cout << "\t 3 Shto kamion: " << endl;
+    cout << "\t 4 Dil: " << endl;
+    cout << " Zgjidhni: ";
 }
 
 int main()
@@ -87,6 +87,7 @@ int main()
             cin >> z;
             cout << "Jep vitin e prodhimit: " << endl;
             cin >> a;
+            cout << endl;
             {
                 Makine m1(x, y, z, a);
                 m1.PrintInfo();
@@ -102,6 +103,7 @@ int main()
             cin >> z;
             cout << "Jep vitin e prodhimit: " << endl;
             cin >> a;
+            cout << endl;
             {
                 Motorr mo1(x, y, z, a);
                 mo1.PrintInfo();
@@ -117,6 +119,7 @@ int main()
             cin >> z;
             cout << "Jep vitin e prodhimit: " << endl;
             cin >> a;
+            cout << endl;
             {
                 Kamion k1(x, y, z, a);
                 k1.PrintInfo();
