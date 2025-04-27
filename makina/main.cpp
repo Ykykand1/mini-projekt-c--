578d3987078b99508d38129ffe591b7c15abca92
andi <andikojanaj321@gmail.com>
2025-04-27 22:02:54 +0200

    Add configuration files for Visual Studio Code and enhance main program structure

--- makina/main.cpp
+++ makina/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
-
+#include <vector> // për të përdorur vector
+#include <string>
 using namespace std;
 
+// Klasa bazë për Automjet
 class Automjet
 {
 public:
@@ -12,141 +14,154 @@ public:
 
     Automjet(string emri, string brand, string modeli, int viti)
         : emri(emri), brand(brand), modeli(modeli), viti(viti) {}
-    virtual void PrintInfo();
+
+    virtual void PrintInfo() = 0; // funksion virtual
+    virtual ~Automjet() {}        // destruktor virtual për të liruar kujtesën
 };
 
+// Klasa për Makina
 class Makine : public Automjet
 {
 public:
-    Makine(string emri, string brand, string modeli, int viti) : Automjet(emri, brand, modeli, viti) {}
+    Makine(string emri, string brand, string modeli, int viti)
+        : Automjet(emri, brand, modeli, viti) {}
 
     void PrintInfo() override
     {
-        cout << "--Makina--: " << emri << endl;
-        cout << "--Brand-i--: " << brand << endl;
-        cout << "--Modeli--: " << modeli << endl;
-        cout << "--Viti--: " << viti << endl;
-        cout<<"\n";
+        cout << "Makina: " << emri << ", Brand: " << brand
+             << ", Modeli: " << modeli << ", Viti: " << viti << endl;
     }
 };
 
+// Klasa për Motorra
 class Motorr : public Automjet
 {
 public:
-    Motorr(string emri, string brand, string modeli, int viti) : Automjet(emri, brand, modeli, viti) {}
+    Motorr(string emri, string brand, string modeli, int viti)
+        : Automjet(emri, brand, modeli, viti) {}
 
     void PrintInfo() override
     {
-        cout << "--Motorri--: " << emri << endl;
-        cout << "--Brand-i--: " << brand << endl;
-        cout << "--Modeli--: " << modeli << endl;
-        cout << "--Viti--: " << viti << endl;
-        cout<<"\n";
+        cout << "Motorri: " << emri << ", Brand: " << brand
+             << ", Modeli: " << modeli << ", Viti: " << viti << endl;
     }
 };
 
+// Klasa për Kamionë
 class Kamion : public Automjet
 {
 public:
-    Kamion(string emri, string brand, string modeli, int viti) : Automjet(emri, brand, modeli, viti) {}
+    Kamion(string emri, string brand, string modeli, int viti)
+        : Automjet(emri, brand, modeli, viti) {}
 
     void PrintInfo() override
     {
-        cout << "--Kamion--: " << emri << endl;
-        cout << "--Brand-i--: " << brand << endl;
-        cout << "--Modeli--: " << modeli << endl;
-        cout << "--Viti--: " << viti << endl;
-        cout<<"\n";
+        cout << "Kamion: " << emri << ", Brand: " << brand
+             << ", Modeli: " << modeli << ", Viti: " << viti << endl;
     }
 };
 
+// Funksion për të shfaqur menunë
 void shfaqMenu()
 {
-    cout << "\t---Menuja---"<<endl;
-    cout << "\t 1 Shto makine: " << endl;
-    cout << "\t 2 Shto motor: " << endl;
-    cout << "\t 3 Shto kamion: " << endl;
-    cout << "\t 4 Dil: " << endl;
-    cout<<"\n";
-    cout << " Zgjidhni: ";
+    cout << "\n--- Menuja ---" << endl;
+    cout << "\t1. Shto makine" << endl;
+    cout << "\t2. Shto motor" << endl;
+    cout << "\t3. Shto kamion" << endl;
+    cout << "\t4. Shfaq te gjitha automjetet" << endl;
+    cout << "\t5. Dil" << endl;
+    cout << "Zgjidhni: ";
+}
+
+// Funksion për të marrë të dhënat nga përdoruesi
+void merrTeDhena(string &emri, string &brand, string &modeli, int &viti)
+{
+    cout << "Jep emrin: ";
+    getline(cin, emri);
+    cout << "Jep brand-in: ";
+    getline(cin, brand);
+    cout << "Jep modelin: ";
+    getline(cin, modeli);
+    cout << "Jep vitin e prodhimit: ";
+    cin >> viti;
+    cin.ignore();
+}
+
+// Funksion për të shfaqur të gjithë automjetet e regjistruara
+void shfaqTeGjithaAutomjetet(const vector<Automjet *> &automjetet)
+{
+    if (automjetet.empty())
+    {
+        cout << "Nuk ka automjete te regjistruara!" << endl;
+        return;
+    }
+
+    cout << "\nLista e automjeteve:\n";
+    for (const auto &automjet : automjetet)
+    {
+        automjet->PrintInfo();
+    }
 }
 
 int main()
 {
-    int zgjedhja = 0;
-    bool on = true;
-    while (on)
+    vector<Automjet *> automjetet; // vektor për të ruajtur automjetet
+    int zgjedhja;
+
+    while (true)
     {
         shfaqMenu();
         cin >> zgjedhja;
-        string x, y, z;
-        int a;
+        cin.ignore(); // pastron enter-in pas cin
+
+        if (zgjedhja == 5)
+        {
+            cout << "Duke dal nga programi..." << endl;
+            break;
+        }
+
+        string emri, brand, modeli;
+        int viti;
+
         switch (zgjedhja)
         {
         case 1:
-            cout<<"\n";
-            cout << " Jep emrin: " << endl;
-            cin >> x;
-            cout << " Jep brand-in: " << endl;
-            cin >> y;
-            cout << " Jep modelin: " << endl;
-            cin >> z;
-            cout << " Jep vitin e prodhimit: " << endl;
-            cin >> a;
-            cout << endl;
-            {
-                Makine m1(x, y, z, a);
-                m1.PrintInfo();
-            }
+        {
+            merrTeDhena(emri, brand, modeli, viti);
+            automjetet.push_back(new Makine(emri, brand, modeli, viti)); // ruaj pointerin në vektor
+            cout << "Makina u shtua me sukses!" << endl;
             break;
-
+        }
         case 2:
-            cout<<"\n";
-            cout << " Jep emrin: " << endl;
-            cin >> x;
-            cout << " Jep brand-in: " << endl;
-            cin >> y;
-            cout << " Jep modelin: " << endl;
-            cin >> z;
-            cout << " Jep vitin e prodhimit: " << endl;
-            cin >> a;
-            cout << endl;
-            {
-                Motorr mo1(x, y, z, a);
-                mo1.PrintInfo();
-            }
+        {
+            merrTeDhena(emri, brand, modeli, viti);
+            automjetet.push_back(new Motorr(emri, brand, modeli, viti));
+            cout << "Motorri u shtua me sukses!" << endl;
             break;
-
+        }
         case 3:
-            cout<<"\n";
-            cout << " Jep emrin: " << endl;
-            cin >> x;
-            cout << " Jep brand-in: " << endl;
-            cin >> y;
-            cout << " Jep modelin: " << endl;
-            cin >> z;
-            cout << " Jep vitin e prodhimit: " << endl;
-            cin >> a;
-            cout << endl;
-            {
-                Kamion k1(x, y, z, a);
-                k1.PrintInfo();
-            }
+        {
+            merrTeDhena(emri, brand, modeli, viti);
+            automjetet.push_back(new Kamion(emri, brand, modeli, viti));
+            cout << "Kamioni u shtua me sukses!" << endl;
             break;
-
+        }
         case 4:
-            cout << " Duke dal nga programi..." << endl;
-            return 0;
-
+        {
+            shfaqTeGjithaAutomjetet(automjetet);
+            break;
+        }
         default:
-            cout << " Zgjedhje e gabuar. Provoni serish!!" << endl;
+            cout << "Zgjedhje e gabuar. Provoni perseri!" << endl;
             break;
         }
     }
 
-    return 0;
-}
+    // PASTROJME MEMORIEN
+    for (auto automjet : automjetet)
+    {
+        delete automjet;
+    }
 
-void Automjet::PrintInfo()
-{
+    return 0;
 }
